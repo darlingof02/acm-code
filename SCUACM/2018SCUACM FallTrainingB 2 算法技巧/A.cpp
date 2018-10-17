64d9c379925cc2007933dc97000c9cce5acea10c
darlingof02 <35373874+darlingof02@users.noreply.github.com>
2018-10-17 08:20:55 +0800

    prime greedy
    try on acm

---
+++ SCUACM/2018SCUACM FallTrainingB 2 算法技巧/A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#define ll long long
+using namespace std;
+
+struct sk{
+    double val
+};
+
+int main()
+{
+    int n;
+    while(~scanf("%d", &n))
+    {
+
+    }
+    return 0;
+}
